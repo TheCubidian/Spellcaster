79269495c438c29ca9eae3a2dbfbd4513cb85433
The Cubidian <132101268+TheCubidian@users.noreply.github.com>
2025-09-14 10:17:54 +0000

    Added initial game implementation and difficulty choice

---
+++ Spellcaster.py
@@ -0,0 +1,19 @@
+import random
+import time
+print("""
+               ©Ibrahim Malas
+               █████     █████
+               █████     █████
+                    █████
+                  █████████
+                  █████████
+                  ██     ██
+
+█     █     █  █████  █████     █    ████   ████   █
+ █   █ █   █     █       █     █ █   █   █  █   █  █
+  █ █   █ █      █      █     █████  ████   █   █
+   █     █     █████   █████  █   █  █   █  ████   █\n
+Save the lands with your mighty staff of power! You have a limited number of turns to defeat all of the enemies.
+Each enemy has its type, resistance, and health shown. Also, Wood types are made of inflammable wood.
+Choose your difficulty:
+1 for Easy (2 enemies, 8 rounds) \n2 for Normal (4 enemies, 14 rounds) \n3 for Nightmare (7 enemies, 24 rounds)""")
